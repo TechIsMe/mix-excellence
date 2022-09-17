1e513d20fc471ba2b279f2f92fa5fefb51911f20
bruktawit <bruktawit2wolde@gmail.com>
2022-09-16 21:27:51 -0400

    created file structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-import Nav from './Components/Nav/Nav';
 function App() {
   return (
     <div className="App">
-      <Nav />
-   <h2> hi </h2>
+   <h2> React </h2>
     </div>
   );
 }
