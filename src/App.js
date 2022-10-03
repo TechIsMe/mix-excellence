cda9db22070fb4e56b81e430041890b6a8353abd
bruktawit <bruktawit2wolde@gmail.com>
2022-10-03 14:15:23 -0400

    modified home branch

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import { Route, Routes } from "react-router-dom";
 import AboutUs from "./pages/AboutUs";
 import AdminAboutPage from "./pages/AdminAboutUs";
 import Layout from "./layout/Layout";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Navigation from "./components/navbar/Navigation";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import Navigation from "./components/navbar/Navigation";
 import Banner from "./components/banner/Banner";
 import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/Navbar";
 
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        <Navigation />
+        {/* <Navigation /> */}
+        <Navbar />
         <Banner />
         <Hero />
         <Routes>
--- src/Components/hero/Hero.js
+++ src/Components/hero/Hero.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Hero.css';
 
 function Hero() {
     return (
       <div className='hero-wrapper'>
         <div className='hero'>
-            <img src={process.env.PUBLIC_URL + "/images/Whiskey image.png"} alt="wiskey" />
-            <img src={process.env.PUBLIC_URL + "/images/pngegg 3.png"} alt="wiskey" />
+            <img src={process.env.PUBLIC_URL + "/images/Whiskey.png"} alt="wiskey" />
+            <img src={process.env.PUBLIC_URL + "/images/Green.png"} alt="green margarita" />
         </div>          
     </div>
     )
