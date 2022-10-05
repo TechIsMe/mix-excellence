4e682ce354cd2d428f536a2d398c4016b30d48c1
bruktawit <bruktawit2wolde@gmail.com>
2022-10-05 19:42:31 -0400

    footer file update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import AuthPage from "./pages/AuthPage";
 import { Route, Routes, Navigate } from "react-router-dom";
 import AdminAboutPage from "./admin/aboutus/AdminAboutUs";
 import Layout from "./layout/Layout";
-// import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import AdminMixologistPage from "./admin/mixologist/AdminMixologistPage";
 import Home from "./pages/Home";
