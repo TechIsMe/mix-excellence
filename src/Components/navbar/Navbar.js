dc11920f12967ebeca5d5a187d8db2a2c32a4141
johnson <johnsonle2929@gmail.com>
2022-10-05 16:30:39 -0700

    update

--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -1,41 +1,57 @@
 // import { useRef } from "react";
 // import { FaBars, FaTimes } from "react-icons/fa";
-import './Navbar.css'
+import "./Navbar.css";
 
 function Navbar() {
-	// const navRef = useRef();
-	// const showNavbar = () => {
-	// 	navRef.current.classList.toggle("responsive_nav");
-	// };
+  // const navRef = useRef();
+  // const showNavbar = () => {
+  // 	navRef.current.classList.toggle("responsive_nav");
+  // };
 
-	return (
-		<header>
-			<nav>
-                <ul className="">
-                    <li> <a href="/#">About us</a></li>
-                    <li> <a href="/#">FAQ's</a></li>
-                    <li> <a href="/#">Drinks</a></li>
-                    <li> <a href="/#">Reviews</a></li>
-                </ul>
-                <div>
-                <p class="contact-top" href="/#">Mixcellence</p>
-                </div>
-                <div className="">
-                <a className="contact-left" href="/#">Contact</a>
-                {/* <button className="upbtn"> */}             
-                </div>
-				
-                {/* <button className="btn1">
+  return (
+    <header>
+      <nav>
+        <ul className="">
+          <li>
+            {" "}
+            <a href="#aboutUs">About us</a>
+          </li>
+          <li>
+            {" "}
+            <a href="#faq">FAQ's</a>
+          </li>
+          <li>
+            {" "}
+            <a href="#">Drinks</a>
+          </li>
+          <li>
+            {" "}
+            <a href="#">Reviews</a>
+          </li>
+        </ul>
+        <div>
+          <p class="contact-top" href="/#">
+            Mixcellence
+          </p>
+        </div>
+        <div className="">
+          <a className="contact-left" href="/#">
+            Contact
+          </a>
+          {/* <button className="upbtn"> */}
+        </div>
+
+        {/* <button className="btn1">
                 <p>Contact us</p>
 				</button> */}
-				{/* <button>
+        {/* <button>
 				</button> */}
-			</nav>
-			 {/* <button className="nav-btn" onClick={showNavbar}>
+      </nav>
+      {/* <button className="nav-btn" onClick={showNavbar}>
 				<FaBars />
 			</button>  */}
-		</header>
-	);
+    </header>
+  );
 }
 
 export default Navbar;
--- src/Components/navbar/Navigation.js
+++ src/Components/navbar/Navigation.js
@@ -20,80 +20,80 @@
 //     return () => window.removeEventListener("scroll", onScroll);
 //   }, []);
 
-  const onUpdateActiveLink = (value) => {
-    setActiveLink(value);
-  };
-  return (
-    <div>
-      <Navbar expand="lg" className={scrolled ? "srolled" : ""}>
-        <Container>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link
-                href="#aboutUs"
-                className={
-                  activeLink === "aboutUs"
-                    ? "active navbar-link"
-                    : "navbar-link"
-                }
-                onClick={() => onUpdateActiveLink("aboutUs")}
-              >
-                About us
-              </Nav.Link>
-              <Nav.Link
-                href="#skills"
-                className={
-                  activeLink === "skills" ? "active navbar-link" : "navbar-link"
-                }
-                onClick={() => onUpdateActiveLink("skills")}
-              >
-                FQA's
-              </Nav.Link>
-              <Nav.Link
-                href="#projects"
-                className={
-                  activeLink === "projects"
-                    ? "active navbar-link"
-                    : "navbar-link"
-                }
-                onClick={() => onUpdateActiveLink("projects")}
-              >
-                Drinks
-              </Nav.Link>
-              <Nav.Link
-                href="#projects"
-                className={
-                  activeLink === "projects"
-                    ? "active navbar-link"
-                    : "navbar-link"
-                }
-                onClick={() => onUpdateActiveLink("projects")}
-              >
-                Reviews
-              </Nav.Link>
-              <Nav.Link
-                href="#home"
-                className={
-                  activeLink === "home" ? "active navbar-link" : "navbar-link"
-                }
-                onClick={() => onUpdateActiveLink("home")}
-              >
-                <button className="vvd ">
-                  <span> Mixcellence</span>
-                </button>
-              </Nav.Link>
-            </Nav>
-            <span className="navbar-text">
-              <button className="vvd">
-                <span> Contact</span>
-              </button>
-            </span>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
-  );
-}
+//   const onUpdateActiveLink = (value) => {
+//     setActiveLink(value);
+//   };
+//   return (
+//     <div>
+//       <Navbar expand="lg" className={scrolled ? "srolled" : ""}>
+//         <Container>
+//           <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//           <Navbar.Collapse id="basic-navbar-nav">
+//             <Nav className="me-auto">
+//               <Nav.Link
+//                 href="#aboutUs"
+//                 className={
+//                   activeLink === "aboutUs"
+//                     ? "active navbar-link"
+//                     : "navbar-link"
+//                 }
+//                 onClick={() => onUpdateActiveLink("aboutUs")}
+//               >
+//                 About us
+//               </Nav.Link>
+//               <Nav.Link
+//                 href="#skills"
+//                 className={
+//                   activeLink === "skills" ? "active navbar-link" : "navbar-link"
+//                 }
+//                 onClick={() => onUpdateActiveLink("skills")}
+//               >
+//                 FQA's
+//               </Nav.Link>
+//               <Nav.Link
+//                 href="#projects"
+//                 className={
+//                   activeLink === "projects"
+//                     ? "active navbar-link"
+//                     : "navbar-link"
+//                 }
+//                 onClick={() => onUpdateActiveLink("projects")}
+//               >
+//                 Drinks
+//               </Nav.Link>
+//               <Nav.Link
+//                 href="#projects"
+//                 className={
+//                   activeLink === "projects"
+//                     ? "active navbar-link"
+//                     : "navbar-link"
+//                 }
+//                 onClick={() => onUpdateActiveLink("projects")}
+//               >
+//                 Reviews
+//               </Nav.Link>
+//               <Nav.Link
+//                 href="#home"
+//                 className={
+//                   activeLink === "home" ? "active navbar-link" : "navbar-link"
+//                 }
+//                 onClick={() => onUpdateActiveLink("home")}
+//               >
+//                 <button className="vvd ">
+//                   <span> Mixcellence</span>
+//                 </button>
+//               </Nav.Link>
+//             </Nav>
+//             <span className="navbar-text">
+//               <button className="vvd">
+//                 <span> Contact</span>
+//               </button>
+//             </span>
+//           </Navbar.Collapse>
+//         </Container>
+//       </Navbar>
+//     </div>
+//   );
+// }
 
-// export default Navigation;
+// // export default Navigation;
